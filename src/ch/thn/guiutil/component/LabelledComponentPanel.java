af01227d98e42b35dd60dab81fc49dc29b1092e5
thnaeff <thnaeff@bluewin.ch>
2015-05-21 21:10:03 -0400

    All components top aligned and not spread all across the panel

--- src/ch/thn/guiutil/component/LabelledComponentPanel.java
+++ src/ch/thn/guiutil/component/LabelledComponentPanel.java
@@ -29,8 +29,8 @@ import javax.swing.JPanel;
 import javax.swing.JSeparator;
 
 /**
- * A panel with a specific number of columns where label-component pairs can be 
- * added to. It also offers parameters to configure the vertical, horizontal and 
+ * A panel with a specific number of columns where label-component pairs can be
+ * added to. It also offers parameters to configure the vertical, horizontal and
  * column space and if there should be divider lines shown.
  * 
  * 
@@ -39,23 +39,25 @@ import javax.swing.JSeparator;
  */
 public class LabelledComponentPanel extends JPanel {
 	private static final long serialVersionUID = 6955100393791654848L;
-		
+
 	private static int COL_WIDTH = 4;
 	private static int LABEL = 0;
 	private static int COMPONENT = 1;
 	private static int SPACERVERTICAL = 2;
-	
+
+	private JPanel pContent = null;
+
 	private int[] rows = null;
-	
+
 	private int hgap = 0;
 	private int vgap = 0;
 	private int colspace = 0;
-	
+
 	private boolean labelAlignmentRight = true;
 	private boolean componentAlignmentRight = false;
 	private boolean verticalDivider = true;
 	private boolean horizontalDivider = true;
-		
+
 	/**
 	 * 
 	 * 
@@ -64,7 +66,7 @@ public class LabelledComponentPanel extends JPanel {
 	public LabelledComponentPanel(int numberOfColumns) {
 		this(numberOfColumns, 5, 2, 10, true, false, false, false);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -75,7 +77,7 @@ public class LabelledComponentPanel extends JPanel {
 	public LabelledComponentPanel(int numberOfColumns, boolean verticalDivider, boolean horizontalDivider) {
 		this(numberOfColumns, 5, 2, 10, true, false, verticalDivider, horizontalDivider);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -88,8 +90,8 @@ public class LabelledComponentPanel extends JPanel {
 	 * @param verticalDivider
 	 * @param horizontalDivider
 	 */
-	public LabelledComponentPanel(int numberOfColumns, int hgap, int vgap, int colspace, 
-			boolean labelAlignmentRight, boolean componentAlignmentRight, 
+	public LabelledComponentPanel(int numberOfColumns, int hgap, int vgap, int colspace,
+			boolean labelAlignmentRight, boolean componentAlignmentRight,
 			boolean verticalDivider, boolean horizontalDivider) {
 		this.hgap = hgap;
 		this.vgap = vgap;
@@ -98,13 +100,16 @@ public class LabelledComponentPanel extends JPanel {
 		this.componentAlignmentRight = componentAlignmentRight;
 		this.verticalDivider = verticalDivider;
 		this.horizontalDivider = horizontalDivider;
-		
+
 		rows = new int[numberOfColumns];
-		
-		setLayout(new GridBagLayout());
-				
+
+		pContent = new JPanel(new GridBagLayout());
+
+		setLayout(new BorderLayout());
+		add(pContent, BorderLayout.NORTH);
+
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -114,7 +119,7 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(String label, JComponent comp) {
 		addLabelComponent(0, new JLabel(label), comp, false);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -124,7 +129,7 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(JLabel label, JComponent comp) {
 		addLabelComponent(0, label, comp, false);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -135,7 +140,7 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(int column, String label, JComponent comp) {
 		addLabelComponent(column, new JLabel(label), comp, false);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -146,7 +151,7 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(int column, JLabel label, JComponent comp) {
 		addLabelComponent(column, label, comp, false);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -157,7 +162,7 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(String label, JComponent comp, boolean fill) {
 		addLabelComponent(0, new JLabel(label), comp, fill);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -168,7 +173,7 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(JLabel label, JComponent comp, boolean fill) {
 		addLabelComponent(0, label, comp, fill);
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -180,41 +185,41 @@ public class LabelledComponentPanel extends JPanel {
 	public void addLabelComponent(int column, String label, JComponent comp, boolean fill) {
 		addLabelComponent(column, new JLabel(label), comp, fill);
 	}
-	
+
 	/**
 	 * 
 	 * 
 	 * @param column
 	 * @param label
 	 * @param comp
-	 * @param fill Ignores left/right alignment and fills entire space with given 
+	 * @param fill Ignores left/right alignment and fills entire space with given
 	 * component
 	 */
 	public void addLabelComponent(int column, JLabel label, JComponent comp, boolean fill) {
-		
+
 		if (column > rows.length - 1) {
 			throw new LabelledComponentPanelError("Column " + column + " out of bounds. " +
 					"Number of available columns: " + rows.length);
 		}
-		
+
 		if (horizontalDivider && rows[column] > 0) {
 			//There is already a row -> add the spacers
-			add(createSeparator(JSeparator.HORIZONTAL), createConstraints(LABEL, column, true));
-			add(createSeparator(JSeparator.HORIZONTAL), createConstraints(COMPONENT, column, true));
-			
+			pContent.add(createSeparator(JSeparator.HORIZONTAL), createConstraints(LABEL, column, true));
+			pContent.add(createSeparator(JSeparator.HORIZONTAL), createConstraints(COMPONENT, column, true));
+
 			if (verticalDivider && column < rows.length - 1) {
-				add(createSeparator(JSeparator.VERTICAL), createConstraints(SPACERVERTICAL, column, true));
+				pContent.add(createSeparator(JSeparator.VERTICAL), createConstraints(SPACERVERTICAL, column, true));
 			}
-			
+
 			rows[column]++;
 		}
-			
-		JPanel p1 = new JPanel(new FlowLayout((labelAlignmentRight ? FlowLayout.RIGHT : FlowLayout.LEFT)));
+
+		JPanel p1 = new JPanel(new FlowLayout(labelAlignmentRight ? FlowLayout.RIGHT : FlowLayout.LEFT));
 		p1.add(label);
-				
+
 		JPanel p2 = new JPanel();
 		p2.setLayout(new BorderLayout());
-				
+
 		if (fill) {
 			p2.add(comp, BorderLayout.CENTER);
 		} else {
@@ -224,19 +229,19 @@ public class LabelledComponentPanel extends JPanel {
 				p2.add(comp, BorderLayout.WEST);
 			}
 		}
-		
-		add(p1, createConstraints(LABEL, column, false));
-		add(p2, createConstraints(COMPONENT, column, false));
-		
+
+		pContent.add(p1, createConstraints(LABEL, column, false));
+		pContent.add(p2, createConstraints(COMPONENT, column, false));
+
 		if (verticalDivider && column < rows.length - 1) {
-			add(createSeparator(JSeparator.VERTICAL), createConstraints(SPACERVERTICAL, column, true));
+			pContent.add(createSeparator(JSeparator.VERTICAL), createConstraints(SPACERVERTICAL, column, true));
 		}
-		
+
 		rows[column]++;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * 
@@ -247,58 +252,58 @@ public class LabelledComponentPanel extends JPanel {
 	 */
 	private GridBagConstraints createConstraints(int x, int column, boolean spacer) {
 		GridBagConstraints c = new GridBagConstraints();
-		
-		boolean label = (x == LABEL);
-		boolean component = (x == COMPONENT);
-		
+
+		boolean label = x == LABEL;
+		boolean component = x == COMPONENT;
+
 		int top = 0;
 		int left = 0;
 		int bottom = 0;
 		int right = 0;
-		
-		c.gridx = x + (column * COL_WIDTH);
+
+		c.gridx = x + column * COL_WIDTH;
 		c.gridy = rows[column];
 		c.gridwidth = 1;
 		c.gridheight = 1;
 
 		if (label || component) {
 			//A label or component position
-			
+
 			c.anchor = label ? GridBagConstraints.WEST : GridBagConstraints.EAST;
 			c.fill = GridBagConstraints.HORIZONTAL;
 
 			if (!component && !spacer) {
 				right = hgap;
 			}
-			
+
 			if (spacer) {
 				top = vgap/2;
 				bottom = vgap/2;
 			}
-			
+
 			//Do not adjust the label width
 			c.weightx = label ? 0 : 1.0;
 			c.weighty = 1.0;
 		} else {
 			//Vertical spacer position
-			
+
 			c.weightx = 0;
 			c.weighty = 0;
 			c.fill = GridBagConstraints.VERTICAL;
-			
+
 			//Distribute the space equally to both sides of the spacer
 			if (spacer) {
 				left = colspace/2;
 				right = colspace/2;
 			}
 		}
-		
+
 		c.insets = new Insets(top, left, bottom, right);
-		
+
 		return c;
 	}
 
-	
+
 	/**
 	 * 
 	 * 
@@ -307,13 +312,13 @@ public class LabelledComponentPanel extends JPanel {
 	 */
 	private JSeparator createSeparator(int orientation) {
 		JSeparator s = new JSeparator(orientation);
-		
+
 		s.setForeground(Color.lightGray);
-		
+
 		return s;
 	}
-	
-	
+
+
 	/*************************************************************************
 	 * 
 	 *
@@ -331,8 +336,8 @@ public class LabelledComponentPanel extends JPanel {
 		public LabelledComponentPanelError(String message) {
 			super(message);
 		}
-		
-		
+
+
 	}
-	
+
 }
